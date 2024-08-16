0f2acdcc7d83d1c6d9699677532b4f87747a8379
Abhishek-Sontakke <sontakkeabhishek27@gmail.com>
2024-08-17 01:11:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Abhishek Sontakke-Enjoing and Learning React
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hay,This is my first React Project
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
